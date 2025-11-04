1ff8c50c7b8f080bb7e4766f5059a8081563d35c
hbishnoi <hbishnoi@stevens.edu>
2025-11-04 05:46:53 +0530

    first commit

---
+++ components/StartupCard.tsx
@@ -0,0 +1,89 @@
+import { cn, formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
+  return (
+    <li className="startup-card group">
+      <div className="flex justify-between items-center;">
+        <p className="font-medium text-[16px] bg-primary-100 px-4 py-2 rounded-full group-hover:bg-white-100;">
+          {formatDate(_createdAt)}
+        </p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span>{views}</span>
+        </div>
+      </div>
+      <div className="flex justify-between items-center; mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${author?._id}`}>
+            <p className="font-medium text-[16px] text-black; line-clamp-1">
+              {author?.name}
+            </p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+        <Link href={`/user/${author?._id}`}>
+          <Image
+            src={author?.image!}
+            alt={author?.name!}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="font-normal text-[16px] line-clamp-2 my-3 text-black-100 break-all;">
+          {description}
+        </p>
+        <img
+          src={image}
+          alt="placeholder"
+          className="w-full h-[164px] rounded-[10px] object-cover;"
+        />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category?.toLowerCase()}`}>
+          <p className="font-medium text-[16px] text-black;">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export const StartupCardSkeleton = () => {
+  return (
+    <>
+      {[0, 1, 2, 3, 4].map((index: number) => (
+        <li key={cn("skeleton, index")}>
+          <Skeleton className="startup-card_skeleton" />
+        </li>
+      ))}
+    </>
+  );
+};
+
+export default StartupCard;
---
+++ tailwind.config.ts
@@ -0,0 +1,77 @@
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+import animate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
+
+const config: Config = {
+  // purge: [],
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sanity/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: "475px",
+      },
+      colors: {
+        primary: {
+          100: "#FFE8F0",
+          DEFAULT: "#EE2B69",
+        },
+        secondary: "#FBE843",
+        black: {
+          100: "#333333",
+          200: "#141413",
+          300: "#7D8087",
+          DEFAULT: "#000000",
+        },
+        white: {
+          100: "#F7F7F7",
+          DEFAULT: "#FFFFFF",
+        },
+      },
+      fontFamily: {
+        sans: ["font-work-sans"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        100: "2px 2px 0px 0px rgb(0, 0, 0)",
+        200: "2px 2px 0px 2px rgb(0, 0, 0)",
+        300: "2px 2px 0px 2px rgb(238, 43, 105)",
+      },
+    },
+  },
+  // variants: {
+  //   extend: {},
+  // },
+  // plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  // plugins: {
+  //   typography: require("@tailwindcss/typography"),
+  //   animate: require("tailwindcss-animate"),
+  // },
+  plugins: [
+    animate,
+    typography,
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        ".pattern": {
+          backgroundImage:
+            "linear-gradient(to right, transparent 49.5%, rgba(251,232,67,0.2) 49.5%, rgba(251,232,67,0.6) 50.5%, transparent 50.5%)",
+          backgroundSize: "5% 100%",
+          backgroundPosition: "center",
+          backgroundRepeat: "repeat-x",
+        },
+      });
+    }),
+  ],
+};
+
+export default config;
